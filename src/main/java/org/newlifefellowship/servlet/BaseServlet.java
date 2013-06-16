b5de0557446614a69f5781cf5ff0782b9cd31f97
Jeffrey Mezic <jeffrey.mezic@gmail.com>
2013-06-16 17:40:05 -0400

    Added some javascript libraries. Started writing down the requirements.

---
+++ src/main/java/org/newlifefellowship/servlet/BaseServlet.java
@@ -0,0 +1,11 @@
+package org.newlifefellowship.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BaseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+}
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello Food Pantry World!</h2>
-</body>
-</html>
