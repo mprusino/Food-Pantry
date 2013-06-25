3175107f4e8797289a954789dae50de361e57648
Jeffrey Mezic <jeffrey.mezic@gmail.com>
2013-06-24 23:15:28 -0400

    Re-arranged the project again to be a single project.

--- src/main/java/org/newlifefellowship/services/ZipCodes.java
+++ src/main/java/org/newlifefellowship/services/ZipCodes.java
@@ -2,15 +2,15 @@ package org.newlifefellowship.services;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/zipcodes")
 public class ZipCodes {
-
-    @GET
+    @GET @Path("/{zipcode}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getZipCodes() {
-        return "zip codes!";
+    public String getZipCodes(@PathParam("zipcode") final String zipcode) {
+        return "query: " + zipcode;
     }
 }
